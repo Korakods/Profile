012ff57988c39bd808d47bdfafb39c39eac7212b
Korakod Khawnuan <614259019@webmail.npru.ac.th>
2020-11-09 22:53:59 +0700

    Commit by Korakod

---
+++ data.js
@@ -0,0 +1,12 @@
+const data={
+    "name" : "Korakod Khawnuan",
+    "work" : "อายุ 20 ศึกษาชั้นประถมศึกษาที่ โรงเรียนอรุณวิทยา ศึกษาชั้นมัธยมศึกษาที่ โรงเรียนทับสะแกวิทยา ศึกษาชั้นอุดศึกษาที่ มหาลัยราชภัฏนครปฐม",
+    "aboutme" : {
+            "fullname" : "Korakod Khawnuan",
+            "Phone" : "065-2209334",
+            "Email" : "614259019@webmail.npru.ac.th",
+            "Facebook" : " Kn Korakod ",
+            "Address" : "7/5 ม.1 ต.เขาล้าน อ.ทับสะแก จ.ประจวบคีรีขันธ์"
+    }
+};
+module.exports = data;
